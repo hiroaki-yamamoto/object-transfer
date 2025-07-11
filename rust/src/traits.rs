55fbe2031cebef4b025cd313ae9427844ba52565
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-07-11 09:34:08 +0900

    feat: ✨ Add AckTrait

--- rust/src/traits.rs
+++ rust/src/traits.rs
@@ -10,6 +10,11 @@ pub trait PubTrait {
     T: Serialize + Send + Sync;
 }
 
+#[async_trait]
+pub trait AckTrait {
+  async fn ack(&self) -> Result<(), Error>;
+}
+
 pub trait SubTrait<T>: Stream<Item = Result<T, Error>> + Send {}
 
 impl<T, S> SubTrait<T> for S where S: Stream<Item = Result<T, Error>> + Send {}
