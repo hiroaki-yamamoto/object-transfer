c2764a02a55b7c74259a361805e5a2d098d3ebc3
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-09-01 10:57:59 +0900

    thinking:  🤔 I'm thinking how to make Stream&Message injectable

--- rust/src/nats.rs
+++ rust/src/nats.rs
@@ -3,6 +3,7 @@ pub mod impl_ctx;
 pub mod options;
 pub mod r#pub;
 pub mod sub;
+mod sub_fetcher;
 
 pub use options::AckSubOptions;
 pub use r#pub::Pub;
---
+++ rust/src/nats/sub_fetcher.rs
@@ -0,0 +1,5 @@
+use ::async_nats::jetstream::context::Context;
+
+use super::options::AckSubOptions;
+
+pub struct SubFetcher {}
