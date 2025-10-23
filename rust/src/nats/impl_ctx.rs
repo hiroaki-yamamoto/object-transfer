c772c6cebb5e8f6a75cda153d88c3df082323c5d
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-10-23 09:54:48 +0900

    feat: ✨ Implement PushMsgs

--- rust/src/nats/impl_ctx.rs
+++ rust/src/nats/impl_ctx.rs
@@ -1,12 +1,11 @@
 use ::async_nats::jetstream::Context;
 use ::async_nats::jetstream::consumer::{
-  pull::Stream as PullMsgs, push::Messages,
+  pull::Stream as PullMsgs, push::Messages as PushMsgs,
 };
 use ::async_trait::async_trait;
 use ::bytes::Bytes;
 use ::futures::{Stream, TryFutureExt, TryStreamExt};
 
-use crate::ack_noop::AckNoop;
 use crate::error::Error;
 use crate::traits::{AckTrait, PubCtxTrait, SubCtxTrait};
 
@@ -18,19 +17,27 @@ impl PubCtxTrait for Context {
   }
 }
 
-#[async_trait]
-impl SubCtxTrait for PullMsgs {
-  async fn subscribe(
-    self,
-  ) -> Result<
-    impl Stream<Item = Result<(Bytes, impl AckTrait), Error>> + Send + Sync,
-    Error,
-  > {
-    let messages = self.map_err(Error::from).and_then(async |msg| {
-      let (msg, acker) = msg.split();
-      return Ok((msg.payload.clone(), acker));
-    });
-    Ok(messages)
-  }
+macro_rules! impl_sub_ctx_trait {
+    ($cls_name: ty) => {
+        #[async_trait]
+        impl SubCtxTrait for $cls_name {
+          async fn subscribe(
+            self,
+          ) -> Result<
+            impl Stream<Item = Result<(Bytes, impl AckTrait), Error>> + Send + Sync,
+            Error,
+          > {
+            let messages = self.map_err(Error::from).and_then(async |msg| {
+              let (msg, acker) = msg.split();
+              return Ok((msg.payload.clone(), acker));
+            });
+            Ok(messages)
+          }
+        }
+    };
 }
+
+impl_sub_ctx_trait!(PullMsgs);
+impl_sub_ctx_trait!(PushMsgs);
+
 // TODO: Need idea to implement SubCtxTrait for Ordered Message
