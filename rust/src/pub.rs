0768a2d1204dda1d57199f0e3c5ac7c5e4adaba1
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-12-05 15:47:22 +0900

    refactor: ♻️ Refactor test code

--- rust/src/pub.rs
+++ rust/src/pub.rs
@@ -61,43 +61,39 @@ mod tests {
   use ::rmp_serde::to_vec as to_msgpack;
   use ::serde_json::to_vec as jsonify;
 
+  use crate::r#enum::Format;
   use crate::tests::entity::TestEntity;
   use crate::traits::MockPubCtxTrait;
 
   use super::*;
 
-  #[tokio::test]
-  async fn test_publish_json() {
-    let entity = TestEntity::new(1, "Test Name");
-    let subject = "test.subject.json";
-    let correct = Bytes::from(jsonify(&entity).unwrap());
+  async fn test_publish(format: Format) {
+    let entity = TestEntity::new(1, &format!("Test Name: {:?}", format));
+    let subject = &format!("test.subject.{:?}", format);
+    let correct = Bytes::from(match format {
+      Format::MessagePack => to_msgpack(&entity).unwrap(),
+      Format::JSON => jsonify(&entity).unwrap(),
+    });
     let mut ctx = MockPubCtxTrait::new();
     ctx
       .expect_publish()
-      .with(eq(subject), eq(correct))
+      .with(eq(subject.clone()), eq(correct))
       .times(1)
       .returning(|_, _| Ok(()));
     let publisher: Pub<TestEntity> =
-      Pub::new(Arc::new(ctx), subject, Format::JSON).unwrap();
+      Pub::new(Arc::new(ctx), subject, format).unwrap();
     let res = publisher.publish(&entity).await;
     assert!(res.is_ok());
   }
 
   #[tokio::test]
+  async fn test_publish_json() {
+    test_publish(Format::JSON).await;
+  }
+
+  #[tokio::test]
   async fn test_publish_msgpack() {
-    let entity = TestEntity::new(1, "Test Name");
-    let subject = "test.subject.msgpack";
-    let correct = Bytes::from(to_msgpack(&entity).unwrap());
-    let mut ctx = MockPubCtxTrait::new();
-    ctx
-      .expect_publish()
-      .with(eq(subject), eq(correct))
-      .times(1)
-      .returning(|_, _| Ok(()));
-    let publisher: Pub<TestEntity> =
-      Pub::new(Arc::new(ctx), subject, Format::MessagePack).unwrap();
-    let res = publisher.publish(&entity).await;
-    assert!(res.is_ok());
+    test_publish(Format::MessagePack).await;
   }
 
   #[tokio::test]
