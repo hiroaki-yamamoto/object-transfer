3ae4bb653cd8429305675c799683341e6081a0ef
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2025-12-13 12:46:47 +0900

    docs: 📝 Add lib doc for rustdoc

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,18 @@
+//! This library provides a simple and efficient way to transfer objects between
+//! different parts of an application or between different applications through
+//! message brokers like NATS.
+//! It supports serialization and/or deserialization of various data formats,
+//! making it easy to send and/or receive complex data structures.
+//!
+//! # CI/CD Status
+//!
+//! | Service | Status |
+//! |---------|--------|
+//! | Rust Code Test | [![Test Rust Code Img]][Test Rust Code] |
+//!
+//! [Test Rust Code Img]: https://github.com/hiroaki-yamamoto/object-transfer/actions/workflows/test_rust.yml/badge.svg
+//! [Test Rust Code]: https://github.com/hiroaki-yamamoto/object-transfer/actions/workflows/test_rust.yml
+
 mod ack_noop;
 pub mod r#enum;
 pub mod error;
