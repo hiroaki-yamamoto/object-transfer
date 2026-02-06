7eb63a5d135e94e26b8ed0dd2333ddc656763e92
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2026-02-06 10:29:34 +0900

    feat: 🚧 Start implementing SubCtxTrait
    I want to get motivated...

--- rust/src/redis/subscriber.rs
+++ rust/src/redis/subscriber.rs
@@ -1,4 +1,13 @@
+use ::std::sync::Arc;
+
+use ::async_stream::try_stream;
+use ::async_trait::async_trait;
+use ::bytes::Bytes;
+use ::redis::AsyncCommands;
 use ::redis::aio::MultiplexedConnection;
+
+use crate::errors::SubError;
+use crate::traits::{AckTrait, SubCtxTrait};
 
 use super::config::SubscriberConfig;
 
@@ -6,4 +15,24 @@ use super::config::SubscriberConfig;
 pub struct Subscriber {
   con: MultiplexedConnection,
   cfg: SubscriberConfig,
+}
+
+impl Subscriber {
+  pub fn new(con: &MultiplexedConnection, cfg: SubscriberConfig) -> Self {
+    Self {
+      con: con.clone(),
+      cfg,
+    }
+  }
+}
+
+#[async_trait]
+impl SubCtxTrait for Subscriber {
+  async fn subscribe(
+    &self,
+  ) -> Result<
+    BoxStream<Result<(Bytes, Arc<dyn AckTrait + Send + Sync>), SubError>>,
+    SubError,
+  > {
+  }
 }
